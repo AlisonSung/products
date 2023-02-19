f5d9e65d3df3bd3e921c3ba433200b1f1bfd32de
Alison Sung <n9607405@gmail.com>
2023-02-19 17:05:59 +0800

    add check file exist

--- products.py
+++ products.py
@@ -1,5 +1,19 @@
+import os #operating system
+
+products = []
+if os.path.isfile('products.csv'):
+    print('yeah!')
+    with open ('prodcts.csv', 'r', encoding = 'utf-8') as f:
+        for line in f :
+            if '商品,價格'in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+else:
+    print('找不到')
+
  #read file
- products = []
  with open ('prodcts.csv', 'r', encoding = 'utf-8')
      for line in f :
         if '商品,價格'in line:
